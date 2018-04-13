781a5e51b9f6dcca01972dfb1902aa90476ad67d
Xingli23 <xinglee23@outlook.com>
2018-04-13 23:43:51 +0800

    chat

--- server/user.js
+++ server/user.js
@@ -14,12 +14,18 @@ Router.get('/list', function(req, res) {
 })
 Router.get('/getmsglist', function(req, res) {
   const userid = req.cookies.userid;
-  // {'$or':[{from:user, to: user}]}
-  Chat.find({}, function(err, doc) {
-    if(!err) {
-      return res.json({code: 0, msg: doc});
-    }
+  User.find({}, function(e, userdoc) {
+    let users = {};
+    userdoc.forEach(v=>{
+      users[v._id] = {name:v.user, avatar:v.avatar}
+    })
+    Chat.find({'$or':[{from:user},{to:user}]}, function(err, doc) {
+      if(!err) {
+        return res.json({code: 0, msg: doc, users:users});
+      }
+    }) 
   })
+  // {'$or':[{from:user, to: user}]}
 })
 Router.post('/update', function(req, res) {
   const userid = req.cookies.userid;
